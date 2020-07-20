cd78fe9fef1e2db4f26b8cb8637fcbe22f844edf
Alois Sečkár <ellrohir@seznam.cz>
2020-07-20 19:39:23 +0200

    Supplies generator fix
    Sometimes "bound must be positive" error was thrown

--- src/galaconomy/universe/economy/EconomyManager.java
+++ src/galaconomy/universe/economy/EconomyManager.java
@@ -10,7 +10,11 @@ public class EconomyManager {
         for (Star star : stars) {
             for (Supplies current : star.getSupplies().values()) {
                 
-                int amountDelta = rand.nextInt(current.getAmount() / 10 + 1) + 1;
+                int amount = current.getAmount();
+                if (amount < 1) {
+                    amount = 1;
+                }
+                int amountDelta = rand.nextInt(amount / 10 + 1) + 1;
                 if (rand.nextBoolean()) {
                     current.increaseAmount(amountDelta);
                     current.setPriceBuy(Math.max(current.getPriceBuy() - rand.nextInt(amountDelta), 101));
