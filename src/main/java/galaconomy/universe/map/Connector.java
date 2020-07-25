9db96e776f55f58ceb47ac524ad01a2dc11e3fd0
Alois Sečkár <ellrohir@seznam.cz>
2020-07-25 19:39:30 +0200

    Update Connector.java
    Connector now knows the distance between points

--- src/main/java/galaconomy/universe/map/Connector.java
+++ src/main/java/galaconomy/universe/map/Connector.java
@@ -1,18 +1,26 @@
 package galaconomy.universe.map;
 
 import galaconomy.constants.Constants;
+import galaconomy.utils.MathUtils;
 import java.awt.Color;
 
 public class Connector extends AbstractMapElement {
     
     private final Star point1;
     private final Star point2;
+    private final double distance;
 
     public Connector(Star point1, Star point2) throws Exception {
         super("Connector", "Permanent rift connection between two systems", Constants.FOLDER_IMG + "rift_gate.png", Color.GRAY, 0, 0);
         if (point1 != null && point2 != null) {
             this.point1 = point1;
             this.point2 = point2;
+            
+            int x1 = point1.getX();
+            int y1 = point1.getY();
+            int x2 = point2.getX();
+            int y2 = point2.getY();
+            this.distance = MathUtils.getDistance(x1, y1, x2, y2);
         } else {
             throw new Exception("Connected points cannot be NULL");
         }
@@ -24,6 +32,10 @@ public class Connector extends AbstractMapElement {
 
     public Star getPoint2() {
         return point2;
+    }
+
+    public double getDistance() {
+        return distance;
     }
 
     @Override
